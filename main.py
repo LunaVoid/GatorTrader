bd3e956936279b231fcf4345709d387e02916363
Luna <26190850+LunaVoid@users.noreply.github.com>
2025-02-07 11:18:01 -0500

    working backend connection

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-'''this is where our backend will be written it is in 
+'''this is where our backend will be written it is in
 the main folder because it makes it easier to serve the react app'''
+import os
+from flask import Flask, send_from_directory
+
+
+app = Flask(__name__, static_folder='./GatorTraderFrontend/dist', static_url_path='/')
+
+@app.route("/")
+def serveReact():
+    print("Sending home page")
+    return app.send_static_file('index.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run()
