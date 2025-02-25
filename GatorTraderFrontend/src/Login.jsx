0f686a30aec764f1d84de8fedd081cd77c093c18
pvanover <117866578+pvanover@users.noreply.github.com>
2025-02-25 15:31:38 -0500

    quick

--- GatorTraderFrontend/src/Login.jsx
+++ GatorTraderFrontend/src/Login.jsx
@@ -25,7 +25,7 @@ function Login() {
           <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet"></link>
             
       </head>
-      {(w) &&<Navbar/>}
+      {(true) &&<Navbar/>}
       <div className = "logo-container">
                 <img className="logopic" src= "../img/gatortradertransparent.png"></img>
                 <p className = "logostuff"> GatorTrader</p>
