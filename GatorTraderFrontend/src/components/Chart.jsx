4986c4afdf59615c7d61df36268cbfc2350d1ec3
pvanover <117866578+pvanover@users.noreply.github.com>
2025-03-09 13:10:43 -0400

    getting the chart positioned correctly

--- GatorTraderFrontend/src/components/Chart.jsx
+++ GatorTraderFrontend/src/components/Chart.jsx
@@ -8,13 +8,13 @@ import { curveMonotoneX } from "d3-shape";
 
 const AreaChart = ({ ratio, type }) => {
     const [data, setData] = useState([]);
-    const [chartWidth, setCharWidth] = useState(window.innerWidth * 0.8);
+    const [chartWidth, setCharWidth] = useState(Math.min(window.innerWidth * 0.8, 800));
 
     useEffect(() => {
         setData(getLocalStockData());
 
         const handleResize = () => {
-            setCharWidth(window.innerWidth * 0.8)
+            setCharWidth(Math.min(window.innerWidth * 0.8, 800));
         };
 
         window.addEventListener("resize", handleResize);
@@ -22,41 +22,46 @@ const AreaChart = ({ ratio, type }) => {
     }, []);
 
     return data.length > 0 ? (
-        <ChartCanvas
-        ratio={ratio}
-        width={chartWidth}
-        height={400}
-        margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
-        seriesName="NVDA"
-        data={data}
-        type={type}
-        xAccessor={(d) => d.date}
-        xScale={scaleTime()}
-        xExtents={[data[0]?.date, data[data.length - 1]?.date]}
-    >
-        <Chart id={0} yExtents={[(d) => d.close]}>
-            <defs>
-                <linearGradient id="myGradient" x1="0" y1="100%" x2="0" y2="0%">
-                    <stop offset="5%" stopColor="#00c853" stopOpacity={0.2} />
-                    <stop offset="70%" stopColor="#00b040" stopOpacity={0.4} />
-                    <stop offset="100%" stopColor="#008000" stopOpacity={0.8} />
-                </linearGradient>
-            </defs>
-            <XAxis axisAt="bottom" orient="bottom" ticks={6} />
-            <YAxis axisAt="left" orient="left" />
-            <AreaSeries
-                yAccessor={(d) => d.close}
-                fill="url(#myGradient)"
-                stroke="#00b040"
-                strokeWidth={2}
-                interpolation={curveMonotoneX}
-            />
-        </Chart>
-    </ChartCanvas>
-) : (
-    <p>Loading data...</p>
+        <div className = "chart-wrapper">
+            <ChartCanvas
+            ratio={ratio}
+            width={chartWidth}
+            height={400}
+            margin={{ left: 100, right: 20, top: 10, bottom: 30 }}
+            seriesName="NVDA"
+            data={data}
+            type={type}
+            xAccessor={(d) => d.date}
+            xScale={scaleTime()}
+            xExtents={[data[0]?.date, data[data.length - 1]?.date]}
+        >
+            <Chart id={0} yExtents={[(d) => d.close]}>
+                <defs>
+                    <linearGradient id="myGradient" x1="0" y1="100%" x2="0" y2="0%">
+                        <stop offset="5%" stopColor="#00c853" stopOpacity={0.2} />
+                        <stop offset="70%" stopColor="#00b040" stopOpacity={0.4} />
+                        <stop offset="100%" stopColor="#008000" stopOpacity={0.8} />
+                    </linearGradient>
+                </defs>
+                <XAxis axisAt="bottom" orient="bottom" ticks={6} />
+                <YAxis axisAt="left" orient="left" />
+                <AreaSeries
+                    yAccessor={(d) => d.close}
+                    fill="url(#myGradient)"
+                    stroke="#00b040"
+                    strokeWidth={2}
+                    interpolation={curveMonotoneX}
+                />
+            </Chart>
+        </ChartCanvas>
+        </div>
+        ) : (
+        <p>Loading data...</p>
 
     );
+
+        
+        
 
 };
 
