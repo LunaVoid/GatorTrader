395d68dffbc7aba4ff05f4a4c549a8c8afe24b65
hiralshukla <hiralshukla365@gmail.com>
2025-03-09 17:24:36 -0400

    Change AreaChart color to Green
    learned how to change attributes of areachart in react-financial-charts using their documentation and sandboxes

--- GatorTraderFrontend/src/components/Chart.jsx
+++ GatorTraderFrontend/src/components/Chart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { getLocalStockData } from "../utils/dataUtil";
 import {ChartCanvas, Chart} from "react-financial-charts";
 import { AreaSeries } from "react-financial-charts";
@@ -6,9 +6,12 @@ import { XAxis, YAxis } from "react-financial-charts";
 import { scaleTime, scaleLinear } from "d3-scale";
 import { curveMonotoneX } from "d3-shape";
 
+
 const AreaChart = ({ ratio, type }) => {
     const [data, setData] = useState([]);
     const [chartWidth, setCharWidth] = useState(Math.min(window.innerWidth * 0.8, 800));
+    const canvasRef = useRef(null);
+    const graidentRef = useRef(null);
 
     useEffect(() => {
         setData(getLocalStockData());
@@ -21,9 +24,19 @@ const AreaChart = ({ ratio, type }) => {
         return () => window.removeEventListener("resize", handleResize);
     }, []);
 
+
+    const createGradient = (ctx, canvas) => {
+        const graident = ctx.createLinearGradient(0, canvas.height, 0, 0);
+        graident.addColorStop(0.05, "rgba(0, 200, 83, 0.2)");  // 5% offset
+        gradient.addColorStop(0.7, "rgba(0, 176, 64, 0.4)");  // 70% offset
+        gradient.addColorStop(1, "rgba(0, 128, 0, 0.8)");    // 100% offset)
+        return gradient;
+    }
+
     return data.length > 0 ? (
         <div className = "chart-wrapper">
             <ChartCanvas
+            ref={canvasRef}
             ratio={ratio}
             width={chartWidth}
             height={400}
@@ -38,17 +51,17 @@ const AreaChart = ({ ratio, type }) => {
             <Chart id={0} yExtents={[(d) => d.close]}>
                 <defs>
                     <linearGradient id="myGradient" x1="0" y1="100%" x2="0" y2="0%">
-                        <stop offset="5%" stopColor="#00c853" stopOpacity={0.2} />
-                        <stop offset="70%" stopColor="#00b040" stopOpacity={0.4} />
-                        <stop offset="100%" stopColor="#008000" stopOpacity={0.8} />
+                        <stop offset="5%" stop-color="#00c853" />
+                        <stop offset="70%" stop-color="#00b040" />
+                        <stop offset="100%" stop-color="#008000"/>
                     </linearGradient>
                 </defs>
                 <XAxis axisAt="bottom" orient="bottom" ticks={6} />
                 <YAxis axisAt="left" orient="left" />
                 <AreaSeries
                     yAccessor={(d) => d.close}
-                    fill="url(#myGradient)"
-                    stroke="#00b040"
+                    strokeStyle="#00b040"
+                    fillStyle="rgba(0, 128, 0, 0.2)"
                     strokeWidth={2}
                     interpolation={curveMonotoneX}
                 />
