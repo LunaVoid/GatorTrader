f8198387d540f282b866a34c3bb10ab2c17fb242
Luna <26190850+LunaVoid@users.noreply.github.com>
2025-03-16 16:00:59 -0400

    working sessionStorage credentials

--- GatorTraderFrontend/src/utils/userContext.jsx
+++ GatorTraderFrontend/src/utils/userContext.jsx
@@ -1,24 +1,36 @@
 import { createContext, useContext, useState } from 'react';
 import { logIn, signUp } from './auth';
+import { useNavigate } from "react-router-dom";
 
 const UserContext = createContext(null);
 
 
+
 export function UserProvider({ children }) {
+    const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [token, setToken] = useState(null);
     const [exp,setEXP] = useState(null);
     
     const loadUser = () =>{
-        userName = sessionStorage.getItem("gtUsername");
-        theToken = sessionStorage.getItem("gtToken");
-        theExpiration = sessionStorage.getItem("gtEXP"); 
-        if(userName && theToken && theExpiration != null){
-            
+        console.log("here")
+        const userName = sessionStorage.getItem("gtUsername");
+        const theToken = sessionStorage.getItem("gtToken");
+        const theExpiration = Number(sessionStorage.getItem("gtEXP")); 
+        console.log(userName)
+        if(userName && theToken && theExpiration != "null"&& !isNaN(theExpiration) && userName && theToken && theExpiration != null){
+            console.log("valid token?")
+            if(Date.now()<theExpiration * 1000){
+                console.log("existing token is valid")
+                setUser(userName);
+                setToken(theToken);
+                setEXP(theExpiration)
+                navigate("/")
+            }
             
         }
         else{
-            console.log("n")
+            console.log("existing token doesn't work, user must login")
         }
     }
 
@@ -30,9 +42,10 @@ export function UserProvider({ children }) {
             setUser(loginData.username);
             setToken(loginData.token);
             setEXP(loginData.exp)
-            sessionStorage.setItem("gtUsername", user);
-            sessionStorage.setItem("gtToken", token); 
-            sessionStorage.setItem("gtEXP", exp); 
+            console.log(user,token,exp)
+            sessionStorage.setItem("gtUsername", loginData.username);
+            sessionStorage.setItem("gtToken", loginData.token); 
+            sessionStorage.setItem("gtEXP", loginData.exp); 
             console.log("State updated - User:", userData.username, "Token:", loginData.token);
 
         } catch (error) {
@@ -44,6 +57,7 @@ export function UserProvider({ children }) {
     const logoutUser = () => {
         setUser(null);
         setToken(null);
+        sessionStorage.clear();
     };
 
     const signupUser = async (userData) => {
@@ -62,7 +76,7 @@ export function UserProvider({ children }) {
 
     
     return (
-        <UserContext.Provider value={{ user, token, loginUser, logoutUser, signupUser }}>
+        <UserContext.Provider value={{ user, token, loginUser, logoutUser, signupUser, loadUser }}>
             {children}
         </UserContext.Provider>
     );
