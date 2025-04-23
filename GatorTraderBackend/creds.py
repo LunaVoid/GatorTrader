bc1eed9bffc66247d90ae244bb6dd32a7ea3a758
Luna <26190850+LunaVoid@users.noreply.github.com>
2025-04-22 22:07:26 -0400

    Update creds.py

--- GatorTraderBackend/creds.py
+++ GatorTraderBackend/creds.py
@@ -7,6 +7,6 @@ def get_db_connection():  # Validates the email
     
     conn = psycopg2.connect(host=os.environ['DB_HOST'],
                             database=os.environ['DB_NAME'],
-                            user=os.environ['DB_USERNAME'],
+                            user=os.environ['DB_USER'],
                             password=os.environ['DB_PASSWORD'])
     return conn
